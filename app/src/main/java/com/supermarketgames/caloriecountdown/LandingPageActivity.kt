5e85b030e7146dc8d2258a1d6c84e5f5ac05f3ec
masongrosko <69281238+masongrosko@users.noreply.github.com>
2021-07-18 01:25:02 -0500

    create basic activity files

--- app/src/main/java/com/supermarketgames/caloriecountdown/LandingPageActivity.kt
+++ app/src/main/java/com/supermarketgames/caloriecountdown/LandingPageActivity.kt
@@ -1,23 +1,44 @@
 package com.supermarketgames.caloriecountdown
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.supermarketgames.caloriecountdown.databinding.ActivityLandingPageBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class LandingPageActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityLandingPageBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Grab values from content view
+        setContentView(R.layout.activity_landing_page)
+        // val newGameButton: Button = findViewById(R.id.newGameButton)
+        val highScoresButton: Button = findViewById(R.id.highScoresButton)
+        val settingsButton: FloatingActionButton = findViewById(R.id.settingsFloatingActionButton)
 
+        // Create intents
+        val highScoresIntent = Intent(
+            this,
+            HighScoresActivity::class.java
+        )
+        val settingsIntent = Intent(
+            this,
+            SettingsActivity::class.java
+        )
+
+        // Set on click listeners
+        // New game button
+        // newGameButton.setOnClickListener
+
+        // High scores button
+        highScoresButton.setOnClickListener {
+            startActivity(highScoresIntent)
+        }
+
+        // Settings button
+        settingsButton.setOnClickListener{
+            startActivity(settingsIntent)
         }
 
     }
---
+++ app/src/main/java/com/supermarketgames/caloriecountdown/SettingsActivity.kt
@@ -0,0 +1,33 @@
+package com.supermarketgames.caloriecountdown
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Grab values from content view
+        setContentView(R.layout.activity_settings)
+        val returnButton: FloatingActionButton = findViewById(
+            R.id.settingsReturnFloatingActionButton
+        )
+
+        // Create intents
+        val landingPageIntent = Intent(
+            this,
+            LandingPageActivity::class.java
+        )
+
+        // Set on click listeners
+        returnButton.setOnClickListener {
+            startActivity(landingPageIntent)
+        }
+    }
+}
