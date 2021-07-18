1aac34d69e5c5a66e8e1052aca4a92ceb2007a29
masongrosko <69281238+masongrosko@users.noreply.github.com>
2021-07-18 01:23:25 -0500

    remove fragments

--- app/src/main/java/com/supermarketgames/caloriecountdown/LandingPageActivity.kt
+++ app/src/main/java/com/supermarketgames/caloriecountdown/LandingPageActivity.kt
@@ -17,24 +17,8 @@ class LandingPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityLandingPageBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_landing_page)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
         }
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_landing_page)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
